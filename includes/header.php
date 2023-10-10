488f8ff44c9dbc9d07b1e9e31d8d98f7267e2230
mooizahmed102 <amoiz6263@gmail.com>
2023-10-11 02:41:37 +0500

    first commit

---
+++ includes/header.php
@@ -0,0 +1,114 @@
+<header class="cs-site_header cs-style1 text-uppercase cs-sticky_header">
+      <div class="cs-main_header">
+        <div class="container">
+          <div class="cs-main_header_in">
+            <div class="cs-main_header_left">
+              <a class="cs-site_branding" href="index/">
+                <img src="assets/img/logo.svg" alt="Logo">
+              </a>
+            </div>
+            <div class="cs-main_header_center">
+              <div class="cs-nav cs-primary_font cs-medium">
+                <ul class="cs-nav_list">
+                  <li class="">
+                    <a href="home/">Home</a>
+                    <!--  -->
+                  </li>
+                  <li>
+                    <a href="about/">About</a>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="service/">Services</a>
+                    <ul>
+                      <li>
+                        <a href="service/">Services</a>
+                      </li>
+                      <li>
+                        <a href="service-details/">Service Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="portfolio/">Portfolio</a>
+                    <ul>
+                      <li>
+                        <a href="portfolio/">Portfolio</a>
+                      </li>
+                      <li>
+                        <a href="portfolio-details/">Portfolio Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="blog/">Blog</a>
+                    <ul>
+                      <li>
+                        <a href="blog/">Blog</a>
+                      </li>
+                      <li>
+                        <a href="blog-details/">Blog Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="shop/">Shop</a>
+                    <ul>
+                      <li>
+                        <a href="shop/">Our Shop</a>
+                      </li>
+                      <li>
+                        <a href="shop-product-details/">Shop Details</a>
+                      </li>
+                      <li>
+                        <a href="shop-cart/">Cart</a>
+                      </li>
+                      <li>
+                        <a href="shop-checkout/">Checkout</a>
+                      </li>
+                      <li>
+                        <a href="shop-order-recived/">Success Order</a>
+                      </li>
+                      <li>
+                        <a href="shop-wishlist/">Wishlist</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href="#">Pages</a>
+                    <ul>
+                      <li>
+                        <a href="contact/">Contact</a>
+                      </li>
+                      <li>
+                        <a href="team/">Team</a>
+                      </li>
+                      <li>
+                        <a href="team-details/">Team Details</a>
+                      </li>
+                      <li>
+                        <a href="case-study/">Case Study Details</a>
+                      </li>
+                      <li>
+                        <a href="faq/">FAQ</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="cs-main_header_right">
+              <div class="cs-toolbox">
+                <span class="cs-icon_btn">
+                  <span class="cs-icon_btn_in">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
